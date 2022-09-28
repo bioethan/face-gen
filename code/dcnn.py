8808c6dee329378a4384f7497e9b5c112586010b
bioethan <ethan.allan.brown@gmail.com>
2022-09-28 13:40:44 +0000

    refactored utils.py

--- code/dcnn.py
+++ code/dcnn.py
@@ -15,7 +15,6 @@ import os
 from PIL import Image
 from torch.autograd import Variable
 from torchvision.utils import save_image, make_grid
-import tqdm
 
 # Global vars 
 # Loading the data
@@ -39,52 +38,6 @@ SAVE_PATH = '/home/ethanbrown/code/results/trial%d/' % TRIAL_NUM
 
 # Modifying images as needed for the size of the neural network 
 DATA_PATH = '/home/ethanbrown/data_general/data/'
-list_data_dirs = os.listdir(DATA_PATH)
-list_data_dirs = [x for x in list_data_dirs if x[0] != '.']
-print('Number of Data Dirs is:', len(list_data_dirs))
-print(list_data_dirs)
-# Option to use large or small images:
-option1 = 's.'
-option2 = 'S.'
-
-# Then transforming images
-pic_iter = 0
-for i in list_data_dirs:
-   if 'BALDUR' in i:
-      list_images = os.listdir(DATA_PATH + i + '/')
-      list_images = [x for x in list_images if option1 in x or option2 in x] 
-      for j in list_images:
-         image = Image.open(DATA_PATH + i + '/' + j, mode='r')
-         image.resize((image_size,image_size), Image.LANCZOS ).convert('RGB').save('./datahigh_jpg/' + 'picnum%d' % pic_iter  + '.jpg')
-         pic_iter = pic_iter + 1
-   elif 'uploads' in i:
-      list_images = os.listdir(DATA_PATH + i + '/')
-      list_images = [x for x in list_images if x[0] != '.']
-      for j in list_images:
-         image = Image.open(DATA_PATH + i + '/' + j, mode='r')
-         image.resize((image_size,image_size), Image.LANCZOS ).convert('RGB').save('./datahigh_jpg/' + 'picnum%d' % pic_iter  + '.jpg')
-         pic_iter = pic_iter + 1
-   elif 'NEXUS' in i:
-      list_dirs = os.listdir(DATA_PATH + i + '/')
-      list_dirs = [x for x in list_dirs if x[0] != '.']
-      for k in list_dirs:
-         next_list_dirs = os.listdir(DATA_PATH + i + '/' + k + '/')
-         next_list_dirs = [x for x in next_list_dirs if x[0] != '.']
-         for l in next_list_dirs:
-            final_list_dirs = os.listdir(DATA_PATH + i + '/' + k + '/' + l + '/')
-            final_list_dirs = [x for x in final_list_dirs if x[0] != '.']
-            for m in final_list_dirs:
-               image = Image.open(DATA_PATH + i + '/' + k + '/' + l + '/' + m + '/Small.png' , mode='r')
-               image.resize((image_size,image_size), Image.LANCZOS ).convert('RGB').save('./datahigh_jpg/' + 'picnum%d' % pic_iter  + '.jpg')
-               pic_iter = pic_iter + 1
-               image.close()
-   elif 'more' in i:
-      list_images = os.listdir(DATA_PATH + i + '/')
-      list_images= [x for x in list_images if x[0] != '.']
-      for j in list_images:
-         image = Image.open(DATA_PATH + i + '/' + j, mode='r')
-         image.resize((image_size,image_size), Image.LANCZOS ).convert('RGB').save('./datahigh_jpg/' + 'picnum%d' % pic_iter  + '.jpg')
-         pic_iter = pic_iter + 1
 
 
 
@@ -95,95 +48,96 @@ print(device)
 
 # DCGAN discriminator class
 class Discriminator(nn.Module):
-    def __init__(self):
-        super(Discriminator, self).__init__()
+   def __init__(self):
+      super(Discriminator, self).__init__()
 
-        # Convolutional block for image discrimination
-        self.conv_block = nn.Sequential(
-            nn.Conv2d(3, 8, 3, 2, 1), 
-            nn.LeakyReLU(0.2), 
+      # Convolutional block for image discrimination
+      self.conv_block = nn.Sequential(
+      nn.Conv2d(3, 8, 3, 2, 1), 
+      nn.LeakyReLU(0.2), 
 
-            nn.Conv2d(8, 16, 3, 2, 1), 
-            nn.LeakyReLU(0.2), 
-            nn.BatchNorm2d(16, 0.8),
+      nn.Conv2d(8, 16, 3, 2, 1), 
+      nn.LeakyReLU(0.2), 
+      nn.BatchNorm2d(16, 0.8),
 
-            nn.Conv2d(16, 32, 3, 2, 1), 
-            nn.LeakyReLU(0.2), 
-            nn.BatchNorm2d(32, 0.8),
+      nn.Conv2d(16, 32, 3, 2, 1), 
+      nn.LeakyReLU(0.2), 
+      nn.BatchNorm2d(32, 0.8),
 
-            nn.Conv2d(32, 64, 3, 2, 1), 
-            nn.LeakyReLU(0.2), 
-            nn.BatchNorm2d(64, 0.8),
+      nn.Conv2d(32, 64, 3, 2, 1), 
+      nn.LeakyReLU(0.2), 
+      nn.BatchNorm2d(64, 0.8),
 
-            nn.Conv2d(64, 128, 3, 2, 1), 
-            nn.LeakyReLU(0.2))
-        # Classification layer
-        self.class_layer = nn.Sequential(nn.Linear(128 * 4 * 4, 1), nn.Sigmoid())
+      nn.Conv2d(64, 128, 3, 2, 1), 
+      nn.LeakyReLU(0.2))
 
-    def forward(self, x):
-        # Resulting convolution over the image
-        out = self.conv_block(x)
-        out = out.view(out.shape[0], -1)
-        # print(out.size())
-        class_out = self.class_layer(out)
+      # Classification layer
+      self.class_layer = nn.Sequential(nn.Linear(128 * 4 * 4, 1), nn.Sigmoid())
 
-        return class_out
+   def forward(self, x):
+      # Resulting convolution over the image
+      out = self.conv_block(x)
+      out = out.view(out.shape[0], -1)
+      # print(out.size())
+      class_out = self.class_layer(out)
+      return class_out
 
 # DCGAN generator class
 class Generator(nn.Module):
-    def __init__(self):
-        super(Generator, self).__init__()
+   def __init__(self):
+      super(Generator, self).__init__()
 
-        # Linear layer to take from the initial size to the 
-        self.latent_reshape = nn.Linear(LATENT_SIZE, gen_init_image * gen_init_image * num_gen_features)
+      # Linear layer to take from the initial size to the 
+      self.latent_reshape = nn.Linear(LATENT_SIZE, gen_init_image * gen_init_image * num_gen_features)
 
-        # Convolutional blocks with upsampling to increase the image size
-        self.conv_block = nn.Sequential(
-            nn.BatchNorm2d(num_gen_features),
-            nn.Upsample(scale_factor=2),
-            nn.Conv2d(num_gen_features, 80, 3, stride=2, padding=1),
+      # Convolutional blocks with upsampling to increase the image size
+      self.conv_block = nn.Sequential(
+      nn.BatchNorm2d(num_gen_features),
+      nn.Upsample(scale_factor=2),
+      nn.Conv2d(num_gen_features, 80, 3, stride=2, padding=1),
 
-            nn.BatchNorm2d(80, 0.8),
-            nn.LeakyReLU(0.2),
-            nn.Upsample(scale_factor=2),
-            nn.Conv2d(80, 64, 3, stride=2, padding=1),
+      nn.BatchNorm2d(80, 0.8),
+      nn.LeakyReLU(0.2),
+      nn.Upsample(scale_factor=2),
+      nn.Conv2d(80, 64, 3, stride=2, padding=1),
 
-            nn.BatchNorm2d(64, 0.8),
-            nn.LeakyReLU(0.2),
-            nn.Upsample(scale_factor=2),
-            nn.Conv2d(64, 20, 2, stride=1, padding=1),
+      nn.BatchNorm2d(64, 0.8),
+      nn.LeakyReLU(0.2),
+      nn.Upsample(scale_factor=2),
+      nn.Conv2d(64, 20, 2, stride=1, padding=1),
 
-            nn.BatchNorm2d(20, 0.8),
-            nn.LeakyReLU(0.2),
-            nn.Upsample(scale_factor=2),
-            nn.Conv2d(20, 3, 3, stride=1, padding=0),
-            
-            nn.Tanh())
+      nn.BatchNorm2d(20, 0.8),
+      nn.LeakyReLU(0.2),
+      nn.Upsample(scale_factor=2),
+      nn.Conv2d(20, 3, 3, stride=1, padding=0),
 
-    def forward(self, x):
-        #print('Input = ', x.size())
-        out = self.latent_reshape(x)
-        #print('Out_linear = ', out.size())
-        out = out.view(out.shape[0], num_gen_features, gen_init_image, gen_init_image)
-        #print('Reshaping Size = ', out.size())
-        img = self.conv_block(out)
-        #print(img.size())
-        return img
-    
+      # Activation Function
+      nn.Tanh())
+
+   def forward(self, x):
+      #print('Input = ', x.size())
+      out = self.latent_reshape(x)
+      #print('Out_linear = ', out.size())
+      out = out.view(out.shape[0], num_gen_features, gen_init_image, gen_init_image)
+      #print('Reshaping Size = ', out.size())
+      img = self.conv_block(out)
+      #print(img.size())
+      return img
+
 # Function to initialize weights as recommended Normal(0, 0.02)
 def weights_init(m):
-    classname = m.__class__.__name__
-    if classname.find('Conv') != -1:
-        nn.init.normal_(m.weight.data, 0.0, 0.02)
-    elif classname.find('BatchNorm') != -1:
-        nn.init.normal_(m.weight.data, 1.0, 0.02)
-        nn.init.constant_(m.bias.data, 0)
+   classname = m.__class__.__name__
+   if classname.find('Conv') != -1:
+      nn.init.normal_(m.weight.data, 0.0, 0.02)
+   elif classname.find('BatchNorm') != -1:
+      nn.init.normal_(m.weight.data, 1.0, 0.02)
+      nn.init.constant_(m.bias.data, 0)
 
 
 # Function for plotting torch normalized images with matplotlib
 def prep_image(img):
-    ret_img = 0.5 *(1 + torch.permute(img, (1, 2, 0)))
-    return ret_img
+   ret_img = 0.5 *(1 + torch.permute(img, (1, 2, 0)))
+   return ret_img
 
 
 # Reading in the images in both folders
@@ -233,52 +187,52 @@ dis_optim = torch.optim.Adam(dis.parameters(), lr=dis_learning_rate, betas=(BETA
 
 # Training loop
 for epoch in range(NUM_EPOCHS):
-    for i, imgs in enumerate(data_loader):
+   for i, imgs in enumerate(data_loader):
 
-        # Loading in the images
-        imgs = imgs[0].to(device)
+      # Loading in the images
+      imgs = imgs[0].to(device)
 
-        # Finding the batch_size
-        batch_size = imgs.shape[0]
+      # Finding the batch_size
+      batch_size = imgs.shape[0]
 
-        # Creating the training zeros and ones
-        true = torch.ones(batch_size, 1).to(device)
-        false = torch.zeros(batch_size, 1).to(device)
+      # Creating the training zeros and ones
+      true = torch.ones(batch_size, 1).to(device)
+      false = torch.zeros(batch_size, 1).to(device)
 
-        #######
-        # Training the generator first
-        gen_optim.zero_grad()
-        z = torch.Tensor(np.random.normal(0, 1, (imgs.shape[0], LATENT_SIZE))).to(device)
+      #######
+      # Training the generator first
+      gen_optim.zero_grad()
+      z = torch.Tensor(np.random.normal(0, 1, (imgs.shape[0], LATENT_SIZE))).to(device)
 
-        # Using the random noise
-        gen_imgs = gen(z)
+      # Using the random noise
+      gen_imgs = gen(z)
 
-        # Finding the loss
-        gen_loss = loss_func(dis(gen_imgs), true)
+      # Finding the loss
+      gen_loss = loss_func(dis(gen_imgs), true)
 
-        #Backpropagating
-        gen_loss.backward()
-        gen_optim.step()
+      #Backpropagating
+      gen_loss.backward()
+      gen_optim.step()
 
-        #######
-        # Now training the discriminator
-        dis_optim.zero_grad()
+      #######
+      # Now training the discriminator
+      dis_optim.zero_grad()
 
-        # Finding the discriminator loss with real and fake data
-        true_loss = loss_func(dis(imgs), true)
-        false_loss = loss_func(dis(gen_imgs.detach()), false)
-        dis_loss = (true_loss + false_loss) / 2
+      # Finding the discriminator loss with real and fake data
+      true_loss = loss_func(dis(imgs), true)
+      false_loss = loss_func(dis(gen_imgs.detach()), false)
+      dis_loss = (true_loss + false_loss) / 2
 
-        # Backpropagating the loss
-        dis_loss.backward()
-        dis_optim.step()
+      # Backpropagating the loss
+      dis_loss.backward()
+      dis_optim.step()
 
-        # Save the results every so many iterations 
-        # Every 10 epochs, print the losses
-        if epoch % 5 == 0 and i == 0:
-          print("Epoch %d/%d -- Dis loss: %f -- Gen loss: %f" % ((epoch), (NUM_EPOCHS), dis_loss.item(), gen_loss.item()))
-          # Save 5x5 rows of images
-          save_image(gen_imgs.data[:25], SAVE_PATH + 'EPOCH%d_ITER%d.png' % ((epoch), i), nrow=5, normalize=True)
+      # Save the results every so many iterations 
+      # Every 10 epochs, print the losses
+      if epoch % 5 == 0 and i == 0:
+         print("Epoch %d/%d -- Dis loss: %f -- Gen loss: %f" % ((epoch), (NUM_EPOCHS), dis_loss.item(), gen_loss.item()))
+         # Save 5x5 rows of images
+         save_image(gen_imgs.data[:25], SAVE_PATH + 'EPOCH%d_ITER%d.png' % ((epoch), i), nrow=5, normalize=True)
 
 torch.save(gen.state_dict(), '/home/ethanbrown/code/models/generator_test%d.pt' % TRIAL_NUM)
 torch.save(dis.state_dict(), '/home/ethanbrown/code/models/discriminator_test%d.pt' % TRIAL_NUM)
--- code/utils.py
+++ code/utils.py
@@ -3,4 +3,65 @@ import math
 import numpy as np
 import os
 from PIL import Image
+from pathlib import Path
+import logging
 
+
+def resize_images(DATA_PATH_STR, IMAGE_SIZE, SAVE_PATH_STR):
+   """
+   resize_image
+   
+   DATA_PATH_str (str): Path that leads to your data folder of choice
+   IMAGE_SIZE (int): The new size of the images that you want to use for 
+   training the DCNN
+   SAVE_PATH (str): Path of a directory that should lead to the folder where you
+   want the images saved. If left empty, defaults to creating such a folder in the 
+   current DATA_PATH directory
+
+   This function should be modified depending on the file structure of image
+   data that you have.
+
+   Returns: A path representation of the folder containing the data for the DCNN modified as needed.
+   """
+
+   DATA_PATH = Path(DATA_PATH_STR)
+
+   # Check to make sure that data path exists
+   assert DATA_PATH.exists() and DATA_PATH.is_dir(),  f'Path to data directory does not exist! Use the correct path for the data!'
+
+   # If the savepath was not entered, then make one in the current directory s
+   if SAVE_PATH_STR == None:
+      SAVE_PATH = Path('./saved_data').mkdir(parents=True, exist_ok=True)
+   else:
+      SAVE_PATH = Path(SAVE_PATH_STR)
+      SAVE_PATH.mkdir(parents=True, exist_ok=True)
+
+   assert len(os.listdir(SAVE_PATH)) < 15, f'Save directory has more than 15 files, and so quitting to avoid overlapping.'
+
+
+   ########
+   #
+   # The specific of the below function will change
+   # depending on the structure of the implementation
+   # of your data.
+   # This is subject to change for the data provided in 
+   # the README.
+   #
+   ########
+
+
+   # Then transforming images
+   pic_iter = 0
+
+   # Using os.walk to gather information about the images present in the data
+   for root, _, files in os.walk(DATA_PATH, topdown=False):
+      for name in files:
+         # Check to make sure things aren't violated (can change based on makeup of data dir)
+         if ('NEXUS' in root and name != 'Small.png') or name[0] == '.' or '.zip' in name:
+            continue
+         image = Image.open(os.path.join(root, name), mode='r')
+         image.resize((IMAGE_SIZE,IMAGE_SIZE), Image.LANCZOS ).convert('RGB').save(SAVE_PATH / f'picnum{pic_iter}.jpg')
+         pic_iter = pic_iter + 1
+         image.close()
+   
+   return SAVE_PATH
